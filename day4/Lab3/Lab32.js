991e8d4d34f58830961100c6c8db96784526f1cc
ngaaltek <nga.altek@gmail.com>
2025-12-19 17:53:47 +0700

    fix something

--- day4/Lab3/Lab32.js
+++ day4/Lab3/Lab32.js
@@ -1,15 +1,17 @@
 let intArray = [1, 2, 3, 4, 5];
-for (i = intArray[0]; i <= intArray.length; i++) {
-    for(j = i;j<=intArray.length;j++){
-    if(i>j){
-        
-        console.log(`${i}`)
-        return 
-    }else {
-        
+let max = findMax();
+console.log(`${max}`)
+function findMax() {
+  for (i = intArray[0]; i <= intArray.length; i++) {
+    for (j = i; j <= intArray.length; j++) {
+      if (i > j) {
+        console.log(`${i}`);
+        return i;
+      } else {
         console.log(`${j}`);
+        return j;
+      }
     }
+  }
+}
 
-    }
-
-} return 
